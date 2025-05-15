0120922e6297dfbd0e6a89b46baf22dc602af262
dushyant <147231160+dushyant010101@users.noreply.github.com>
2025-05-15 16:27:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
